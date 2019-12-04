0bec23f97b51ebb3f4bbb15dcdf5660bd8b0cd40
emilias90 <56910861+emilias90@users.noreply.github.com>
2019-12-04 16:10:41 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+
+This was my 2nd project to complete task during Coderscamp.
+
+# Description
+
+App allows us to check current weather conditions for chosen city.
+It uses AccuWeather API to fetch weather informations.
+
+## Images:
+
+![](screenshots/1.png)
+
+![](screenshots/2.png)
+
+![](screenshots/3.png)
---
+++ webpack.dev.js
@@ -0,0 +1,45 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin'),
+      path = require('path'),
+      merge = require('webpack-merge'),
+      common = require('./webpack.common');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'source-map',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+              loader: 'css-loader',
+              options: {
+                  sourceMap: true
+              }
+          },
+          {
+              loader: 'sass-loader',
+              options: {
+                  sourceMap: true,
+              }
+          }
+      ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html'
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      filename: "[name].css",
+    }),
+  ]
+});
