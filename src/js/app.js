f879a8791348c9f1c4c9e15fc7997ff8b68634d8
emilias90 <56910861+emilias90@users.noreply.github.com>
2019-12-04 17:07:50 +0100

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -3,11 +3,13 @@
 import { welcomeScreen } from './welcomeScreen';
 
 export function app() {
+welcomeScreen();
+  
 window.addEventListener('resize', () => {
   let vh = window.innerHeight * .01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
   });
-  welcomeScreen();
+  
 }
 
 
