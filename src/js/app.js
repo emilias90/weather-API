b112ccfc1347606800a4c77126362f8b0890e401
emilias90 <56910861+emilias90@users.noreply.github.com>
2019-12-04 16:55:17 +0100

    welcomeScreen(); added to app function

--- src/js/app.js
+++ src/js/app.js
@@ -8,6 +8,7 @@ window.addEventListener('resize', () => {
   let vh = window.innerHeight * .01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
+  welcomeScreen();
 }
 
-welcomeScreen();
+
